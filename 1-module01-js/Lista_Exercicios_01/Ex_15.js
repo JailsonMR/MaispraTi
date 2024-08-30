177485522c43b78853110d0b04b0358266653ae1
JailsonMiranda <jailson.comp@gmail.com>
2024-08-29 21:56:40 -0300

    Finalização da aula 01 de html.

--- 1-module01-js/Lista_Exercicios_01/Ex_15.js
+++ 1-module01-js/Lista_Exercicios_01/Ex_15.js
@@ -4,11 +4,37 @@ Fibonacci utilizando um loop for.
 */
 
 let quantidade = 10
-let sequencia = 1
+let sequencia = 0
+let numAnt = 0
+let numAtual = 0
+
 
 for(n = 0; n < quantidade; n++) {
-    sequencia += n
     console.log(sequencia)
+    sequencia++
+    numAnt = sequencia - 1
+    numAtual = sequencia
 }
 
-/// Corrigir
+
+// seq 0
+// numAnt 0
+// seq 1
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
