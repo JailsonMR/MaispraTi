8fe6f88987409ae11561750406c400b7d54940d8
JailsonMiranda <jailson.comp@gmail.com>
2024-11-20 23:54:02 -0300

    Atividade 04

---
+++ 3-reactjs/desfio/script.js
@@ -0,0 +1,35 @@
+function alterar_btn1()
+{
+    document.getElementById('content').innerHTML = `
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    `;
+}
+
+function alterar_btn2()
+{
+    document.getElementById('content').innerHTML = `
+        <ul>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+        </ul>
+    `;
+}
+
+function alterar_btn3()
+{
+    document.getElementById('content').innerHTML = `
+        <ul>
+            <li>9</li>
+            <li>10</li>
+            <li>11</li>
+            <li>12</li>
+        </ul>
+    `;
+}
